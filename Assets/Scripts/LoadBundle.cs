8578a977ce91c0f44d25aa71d187a820e803045c
Sara Nasr <snasr@gsngames.com>
2016-08-29 10:47:38 -0700

    Try again.  Used for reproducing a bug only

---
+++ Assets/Scripts/LoadBundle.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadBundle : MonoBehaviour {
+	public string bundleUrl;
+
+	AssetBundle bundle;
+
+	IEnumerator Start(){
+		WWW www = new WWW (bundleUrl);
+		yield return www;
+		if(www.error != null){
+			throw new System.Exception("There's an error " + www.error);
+		}
+
+		//bundle = www.assetBundle;
+		bundle = AssetBundle.LoadFromMemory(www.bytes);
+	}
+
+	public IEnumerator Spawn(string assetName){
+		if (assetName == "") {
+			Instantiate (bundle.mainAsset);
+		} else {
+			Debug.LogWarning ("Asset Name" + assetName);
+			AssetBundleRequest request= bundle.LoadAssetAsync(assetName, typeof(GameObject));
+			Debug.LogWarning ("Asset Name " + assetName);
+			//yield return new WaitForSeconds(0.5f);
+			/*while(!request.isDone) {
+				//  // TODO: hook up asyncRequest.progress to loading bar animation
+				  //yield return null;
+				}*/
+
+			GameObject obj = request.asset as GameObject;
+
+			GameObject newObject = Instantiate (obj) as GameObject;
+
+			newObject.transform.parent = this.transform;
+
+			Resources.UnloadUnusedAssets ();
+
+		}
+
+		yield return null;
+	}
+
+	public void SpawnInstant(string assetName){
+		if (assetName == "") {
+			Instantiate (bundle.mainAsset);
+		} else {
+			AssetBundleRequest request= bundle.LoadAssetAsync(assetName, typeof(GameObject));
+			Debug.LogWarning ("Asset Name " + assetName);
+
+
+			GameObject obj = request.asset as GameObject;
+
+			GameObject newObject = Instantiate (obj) as GameObject;
+
+			newObject.transform.parent = this.transform;
+
+
+
+			Resources.UnloadUnusedAssets ();
+
+		}
+			
+	}
+
+
+	public IEnumerator SpawnSlowly(){
+		yield return StartCoroutine( Spawn ("GroundSection"));
+		yield return StartCoroutine(Spawn ("QuestionBlock"));
+		yield return StartCoroutine(Spawn ("BrickBackground"));
+		yield return StartCoroutine(Spawn ("DarkGround"));
+		//yield return StartCoroutine(Spawn ("GreenPipe"));
+		yield return StartCoroutine(Spawn ("CloudBackground"));
+		//yield return StartCoroutine(Spawn ("PipeColliderScript"));
+		yield return StartCoroutine(Spawn ("Cube1"));
+		yield return StartCoroutine(Spawn ("Cube"));
+		//yield return StartCoroutine(Spawn ("ThirdPersonController"));
+	}
+
+	public void SpawnInstantly(){
+		SpawnInstant ("GroundSection");
+		SpawnInstant ("QuestionBlock");
+		SpawnInstant ("BrickBackground");
+		SpawnInstant ("DarkGround");
+		//SpawnInstant ("GreenPipe");
+		SpawnInstant ("CloudBackground");
+		//SpawnInstant ("PipeColliderScript");
+		SpawnInstant ("Cube1");
+		SpawnInstant ("Cube");
+		//SpawnInstant ("ThirdPersonController");
+	}
+
+	public void StartSpawn(){
+		StartCoroutine (SpawnSlowly ());
+	}
+}
---
+++ Assets/Scripts/Pipe001Entry.js
@@ -0,0 +1,37 @@
+﻿
+var PipeEntry : GameObject;
+var StoodOn : int;
+
+
+var MainCam : GameObject;
+var SecondCam : GameObject;
+var MainPlayer : GameObject;
+
+function OnTriggerEnter (col : Collider) {
+	StoodOn = 1;
+}
+
+function OnTriggerExit (col : Collider) {
+	StoodOn = 0;
+}
+
+
+function Update () {
+	if (Input.GetButtonDown("GoDown")) {
+		if (StoodOn == 1) {
+			//GameObject.Find("FPSController").GetComponent("FirstPersonController").enabled=false;
+			transform.position = Vector3(0, -1000, 0);
+			WaitingForPipe();
+		}
+	}
+}
+
+function WaitingForPipe () {
+	PipeEntry.GetComponent("Animator").enabled=true;
+	yield WaitForSeconds(2);
+	PipeEntry.GetComponent("Animator").enabled=false;
+	SecondCam.SetActive(true);
+	MainCam.SetActive(false);
+	MainPlayer.transform.position = Vector3(-2.09, -16.75, 13.5);
+	//GameObject.Find("FPSController").GetComponent("FirstPersonController").enabled=true;
+}
